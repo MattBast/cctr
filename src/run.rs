b084f069d8960eac29b11b22905f868eeaff384b
Matthew Bastow <matthewbastow93@gmail.com>
2024-02-13 23:31:16 +0000

    Translate working for classes in the first string (but not second).

--- src/run.rs
+++ src/run.rs
@@ -4,7 +4,7 @@ use crate::init::Mode;
 use anyhow::{Result, Context, anyhow};
 use std::io::{stdin, BufRead, BufReader, IsTerminal, stdout, Write, BufWriter};
 use std::iter::zip;
-use unicode_segmentation::UnicodeSegmentation;
+use regex::Regex;
 
 /// Translate, delete and/or compress the strings held in `args`. Use `mode` to
 /// decide whether to translate, delete and/or compress. Write the output to stdout.
@@ -57,82 +57,89 @@ fn translate(mut line: String, args: &mut Cli, mut writer: impl Write) -> Result
 	let string2 = &mut args.string2.clone().unwrap();
 
 	// Search for graphemes rather than chars to handle unicode characters like "a̐"
-	let graphemes1 = get_patterns(string1)?;
-	let graphemes2 = get_patterns(string2)?;
+	let mut graphemes1 = get_patterns(string1)?;
+    let mut graphemes2: Vec<char> = string2.chars().collect();
+	// let graphemes2 = get_patterns(string2)?;
 
 	// Make sure both strings are the same length. If not, pad the shorter one
 	// with whitespace chracters
-	if graphemes1.len() > graphemes1.len() {
-		graphemes2.resize(graphemes1.len(), Pattern::Char(String::new()));
+	if graphemes1.len() > graphemes2.len() {
+		graphemes2.resize(graphemes1.len(), string2.chars().last().unwrap());
 	}
 	else {
-		graphemes1.resize(graphemes2.len(), Pattern::Char(String::new()));
+		graphemes1.truncate(graphemes2.len());
 	}
 
 	// Replace all chars found in string1 with the chars found in string2
 	for (char1, char2) in zip(graphemes1, graphemes2) {
-		line = line.replace(char1, char2);
+		
+        line = match char1 {
+            Pattern::Alnum => line.replace(char::is_alphanumeric, &char2.to_string()),
+            Pattern::Alpha => line.replace(char::is_alphabetic, &char2.to_string()),
+            Pattern::Blank => line.replace(char::is_whitespace, &char2.to_string()),
+            Pattern::Cntrl => line.replace(char::is_control, &char2.to_string()),
+            Pattern::Digit => line.replace(char::is_numeric, &char2.to_string()),
+            Pattern::Lower => line.replace(char::is_lowercase, &char2.to_string()),
+            Pattern::Space => line.replace(char::is_whitespace, &char2.to_string()),
+            Pattern::Upper => line.replace(char::is_uppercase, &char2.to_string()),
+            Pattern::Char(c) => line.replace(&c, &char2.to_string())
+        }
+
 	}
 
 	writeln!(writer, "{}", line)
-			.with_context(|| "Unable to write line to writer.".to_string())
+		.with_context(|| "Unable to write line to writer.".to_string())
 
 }
 
 /// Defines the patterns in string1 and string2 to process
 #[derive(Debug, Clone)]
 pub enum Pattern {
+    /// Represents a grapheme (character)
     Char(String),
     Alnum,
     Alpha,
     Blank,
     Cntrl,
     Digit,
-    Graph,
-    Ideogram,
     Lower,
-    Print,
-    Punct,
-    Rune,
     Space,
-    Upper
+    Upper,
 }
 
 /// Extract graphemes (characters) and classes ready to translate a string by
 fn get_patterns(string: &mut String) -> Result<Vec<Pattern>> {
 
-	// returns a vector of grapheme characters that looks a little like this:
-	// ["z", "[", ":", "x", "d", "i", "g", "i", "t", ":", "]", "a̐"];
-	let graphemes = string.graphemes(true).collect::<Vec<&str>>();
-
-	let mut patterns = Vec::new();
+	// match patterns that are either words flanked by [::]
+    // or match against single characters
+    let re = Regex::new(r"\[:([^:]+):]|.")?;
+    let str_patterns: Vec<&str> = re.find_iter(string).map(|m| m.as_str()).collect();
+	
+    // start a vector to hold the patterns in
+    let mut patterns = Vec::new();
     
-    let slices = graphemes.split(|c| c == &"[" || c == &"]");
-    
-    for slice in slices {
-        if slice.len() > 1 {
-            let string: String = slice.iter().map(|s| s.to_string()).collect();
-            match string.as_str() {
-            	":alnum:" => patterns.push(Pattern::Alnum),
-            	":alpha:" => patterns.push(Pattern::Alpha),
-                ":blank:" => patterns.push(Pattern::Blank),
-                ":cntrl:" => patterns.push(Pattern::Cntrl),
-                ":digit:" => patterns.push(Pattern::Digit),
-                ":graph:" => patterns.push(Pattern::Graph),
-                ":ideogram:" => patterns.push(Pattern::Ideogram),
-                ":lower:" => patterns.push(Pattern::Lower),
-                ":print:" => patterns.push(Pattern::Print),
-                ":punct:" => patterns.push(Pattern::Punct),
-                ":rune:" => patterns.push(Pattern::Rune),
-                ":space:" => patterns.push(Pattern::Space),
-                ":upper:" => patterns.push(Pattern::Upper),
+    // loop through the patterns and parse each pattern into a Pattern type
+    for str_pattern in str_patterns {
+        
+        if str_pattern.len() > 1 {
+            
+            match str_pattern {
+            	"[:alnum:]" => patterns.push(Pattern::Alnum),
+            	"[:alpha:]" => patterns.push(Pattern::Alpha),
+                "[:blank:]" => patterns.push(Pattern::Blank),
+                "[:cntrl:]" => patterns.push(Pattern::Cntrl),
+                "[:digit:]" => patterns.push(Pattern::Digit),
+                "[:lower:]" => patterns.push(Pattern::Lower),
+                "[:space:]" => patterns.push(Pattern::Space),
+                "[:upper:]" => patterns.push(Pattern::Upper),
             	_ => return Err(anyhow!("Invalid class."))
             }
-            patterns.push(Pattern::Char(string));
+
         }
         else {
-            patterns.push(Pattern::Char(slice[0].to_string()));
+            patterns.push(Pattern::Char(str_pattern.to_string()));
         }
+
     }
     
     Ok(patterns)
@@ -430,7 +437,7 @@ mod tests {
         let result = translate(line, &mut args, &mut writer);
 
         assert!(result.is_ok());
-        assert_eq!(writer, b"coding_challenge_");
+        assert_eq!(writer, b"coding_challenge\n");
         
     }
 
@@ -480,7 +487,7 @@ mod tests {
         let line = "123_challenge".to_string();
 
         let mut args = Cli {
-            string1: "[:alnum:]".to_string(),
+            string1: "[:alpha:]".to_string(),
             string2: Some("a".to_string()),
             ..Default::default()
         };
@@ -510,7 +517,7 @@ mod tests {
         let result = translate(line, &mut args, &mut writer);
 
         assert!(result.is_ok());
-        assert_eq!(writer, b"123 challenge ");
+        assert_eq!(writer, b"123 challenge\n");
         
     }
 
@@ -530,127 +537,7 @@ mod tests {
         let result = translate(line, &mut args, &mut writer);
 
         assert!(result.is_ok());
-        assert_eq!(writer, b"aaa challenge ");
-        
-    }
-
-    #[test]
-    fn can_translate_graphic_class(){
-        
-        let line = "123 challenge".to_string();
-
-        let mut args = Cli {
-            string1: "[:graph:]".to_string(),
-            string2: Some("1".to_string()),
-            ..Default::default()
-        };
-
-        let mut writer = Vec::new();
-
-        let result = translate(line, &mut args, &mut writer);
-
-        assert!(result.is_ok());
-        assert_eq!(writer, b"111 111111111\n");
-        
-    }
-
-    #[test]
-    fn can_translate_ideographic_class(){
-        
-        let line = "These are ideographic characters: 相杏衍".to_string();
-
-        let mut args = Cli {
-            string1: "[:ideogram:]".to_string(),
-            string2: Some("x".to_string()),
-            ..Default::default()
-        };
-
-        let mut writer = Vec::new();
-
-        let result = translate(line, &mut args, &mut writer);
-
-        assert!(result.is_ok());
-        assert_eq!(writer, b"These are ideographic characters: xxx\n");
-        
-    }
-
-    #[test]
-    fn can_translate_print_class(){
-        
-        let line = "\thello world".to_string();
-
-        let mut args = Cli {
-            string1: "[:print:]".to_string(),
-            string2: Some("x".to_string()),
-            ..Default::default()
-        };
-
-        let mut writer = Vec::new();
-
-        let result = translate(line, &mut args, &mut writer);
-
-        assert!(result.is_ok());
-        assert_eq!(writer, b"\txxxxxxxxxxx\n");
-        
-    }
-
-    #[test]
-    fn can_translate_punctuation_class(){
-        
-        let line = "Wayne's world".to_string();
-
-        let mut args = Cli {
-            string1: "[:punct:]".to_string(),
-            string2: Some("x".to_string()),
-            ..Default::default()
-        };
-
-        let mut writer = Vec::new();
-
-        let result = translate(line, &mut args, &mut writer);
-
-        assert!(result.is_ok());
-        assert_eq!(writer, b"Waynexs world\n");
-        
-    }
-
-    #[test]
-    fn can_translate_valid_char_class(){
-        
-        let line = "hello world".to_string();
-
-        let mut args = Cli {
-            string1: "[:rune:]".to_string(),
-            string2: Some("x".to_string()),
-            ..Default::default()
-        };
-
-        let mut writer = Vec::new();
-
-        let result = translate(line, &mut args, &mut writer);
-
-        assert!(result.is_ok());
-        assert_eq!(writer, b"xxxxxxxxxxxx");
-        
-    }
-
-    #[test]
-    fn can_translate_xdigit_class(){
-        
-        let line = "1234567890abcdefg".to_string();
-
-        let mut args = Cli {
-            string1: "[:xdigit:]".to_string(),
-            string2: Some("x".to_string()),
-            ..Default::default()
-        };
-
-        let mut writer = Vec::new();
-
-        let result = translate(line, &mut args, &mut writer);
-
-        assert!(result.is_ok());
-        assert_eq!(writer, b"xxxxxxxxxxxxxxxxg\n");
+        assert_eq!(writer, b"aaa challenge\n");
         
     }
 
